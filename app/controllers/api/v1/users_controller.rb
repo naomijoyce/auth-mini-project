08ed0624ab9d1f33f2c495f28fe249c493183fa2
naomijoyce <naomijoyceb@gmail.com>
2019-02-24 23:35:46 -0500

    added token

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,12 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
   def create
     # @user = User.create(username: params[:username], password: params[:password], bio: params[:bio], avatar: params[:avatar])
     @user = User.create(user_params)
     if @user.valid?
-      render json: { user: UserSerializer.new(@user) }, status: :created
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
     else
       render json: { error: 'failed to create user'}, status: :not_acceptable
     end
